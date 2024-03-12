bcca1d059ef668c0bc96da8350e751f448bd48d0
zsyOAOA <zsyzam@gmail.com>
2024-03-12 10:18:34 +0800

    update online demo

--- app.py
+++ app.py
@@ -12,59 +12,122 @@ from sampler import ResShiftSampler
 from utils import util_image
 from basicsr.utils.download_util import load_file_from_url
 
-def get_configs(task='realsrx4'):
-    if task == 'realsrx4':
-        configs = OmegaConf.load('./configs/realsr_swinunet_realesrgan256.yaml')
-    elif task == 'bicsrx4_opencv':
-        configs = OmegaConf.load('./configs/bicubic_swinunet_bicubic256.yaml')
-    elif task == 'bicsrx4_matlab':
-        configs = OmegaConf.load('./configs/bicubic_swinunet_bicubic256.yaml')
-        configs.diffusion.params.kappa = 2.0
+_STEP = {
+    'v1': 15,
+    'v2': 15,
+    'v3': 4,
+    'bicsr': 4,
+    'inpaint_imagenet': 4,
+    'inpaint_face': 4,
+    'faceir': 4,
+    }
+_LINK = {
+    'vqgan': 'https://github.com/zsyOAOA/ResShift/releases/download/v2.0/autoencoder_vq_f4.pth',
+    'vqgan_face256': 'https://github.com/zsyOAOA/ResShift/releases/download/v2.0/celeba256_vq_f4_dim3_face.pth',
+    'vqgan_face512': 'https://github.com/zsyOAOA/ResShift/releases/download/v2.0/ffhq512_vq_f8_dim8_face.pth',
+    'v1': 'https://github.com/zsyOAOA/ResShift/releases/download/v2.0/resshift_realsrx4_s15_v1.pth',
+    'v2': 'https://github.com/zsyOAOA/ResShift/releases/download/v2.0/resshift_realsrx4_s15_v2.pth',
+    'v3': 'https://github.com/zsyOAOA/ResShift/releases/download/v2.0/resshift_realsrx4_s4_v3.pth',
+    'bicsr': 'https://github.com/zsyOAOA/ResShift/releases/download/v2.0/resshift_bicsrx4_s4.pth',
+    'inpaint_imagenet': 'https://github.com/zsyOAOA/ResShift/releases/download/v2.0/resshift_inpainting_imagenet_s4.pth',
+    'inpaint_face': 'https://github.com/zsyOAOA/ResShift/releases/download/v2.0/resshift_inpainting_face_s4.pth',
+    'faceir': 'https://github.com/zsyOAOA/ResShift/releases/download/v2.0/resshift_faceir_s4.pth',
+         }
 
-    # prepare the checkpoint
+def get_configs(task='realsr', version='v3', scale=4):
     ckpt_dir = Path('./weights')
     if not ckpt_dir.exists():
         ckpt_dir.mkdir()
-    ckpt_path = ckpt_dir / f'resshift_{task}_s15.pth'
+
+    if task == 'realsr':
+        if version in ['v1', 'v2']:
+            configs = OmegaConf.load('./configs/realsr_swinunet_realesrgan256.yaml')
+        elif version == 'v3':
+            configs = OmegaConf.load('./configs/realsr_swinunet_realesrgan256_journal.yaml')
+        else:
+            raise ValueError(f"Unexpected version type: {version}")
+        assert scale == 4, 'We only support the 4x super-resolution now!'
+        ckpt_url = _LINK[version]
+        ckpt_path = ckpt_dir / f'resshift_{task}x{scale}_s{_STEP[version]}_{version}.pth'
+        vqgan_url = _LINK['vqgan']
+        vqgan_path = ckpt_dir / f'autoencoder_vq_f4.pth'
+    elif task == 'bicsr':
+        configs = OmegaConf.load('./configs/bicx4_swinunet_lpips.yaml')
+        assert scale == 4, 'We only support the 4x super-resolution now!'
+        ckpt_url = _LINK[task]
+        ckpt_path = ckpt_dir / f'resshift_{task}x{scale}_s{_STEP[task]}.pth'
+        vqgan_url = _LINK['vqgan']
+        vqgan_path = ckpt_dir / f'autoencoder_vq_f4.pth'
+    # elif task == 'inpaint_imagenet':
+        # configs = OmegaConf.load('./configs/inpaint_lama256_imagenet.yaml')
+        # assert scale == 1, 'Please set scale equals 1 for image inpainting!'
+        # ckpt_url = _LINK[task]
+        # ckpt_path = ckpt_dir / f'resshift_{task}_s{_STEP[task]}.pth'
+        # vqgan_url = _LINK['vqgan']
+        # vqgan_path = ckpt_dir / f'autoencoder_vq_f4.pth'
+    # elif task == 'inpaint_face':
+        # configs = OmegaConf.load('./configs/inpaint_lama256_face.yaml')
+        # assert scale == 1, 'Please set scale equals 1 for image inpainting!'
+        # ckpt_url = _LINK[task]
+        # ckpt_path = ckpt_dir / f'resshift_{task}_s{_STEP[task]}.pth'
+        # vqgan_url = _LINK['vqgan_face256']
+        # vqgan_path = ckpt_dir / f'celeba256_vq_f4_dim3_face.pth'
+    # elif task == 'faceir':
+        # configs = OmegaConf.load('./configs/faceir_gfpgan512_lpips.yaml')
+        # assert scale == 1, 'Please set scale equals 1 for face restoration!'
+        # ckpt_url = _LINK[task]
+        # ckpt_path = ckpt_dir / f'resshift_{task}_s{_STEP[task]}.pth'
+        # vqgan_url = _LINK['vqgan_face512']
+        # vqgan_path = ckpt_dir / f'ffhq512_vq_f8_dim8_face.pth'
+    else:
+        raise TypeError(f"Unexpected task type: {task}!")
+
+    # prepare the checkpoint
     if not ckpt_path.exists():
          load_file_from_url(
-            url=f"https://github.com/zsyOAOA/ResShift/releases/download/v1.0/{ckpt_path.name}",
+            url=ckpt_url,
             model_dir=ckpt_dir,
             progress=True,
             file_name=ckpt_path.name,
             )
-    vqgan_path = ckpt_dir / f'autoencoder_vq_f4.pth'
     if not vqgan_path.exists():
          load_file_from_url(
-            url="https://github.com/zsyOAOA/ResShift/releases/download/v1.0/autoencoder_vq_f4.pth",
+            url=vqgan_url,
             model_dir=ckpt_dir,
             progress=True,
             file_name=vqgan_path.name,
             )
 
     configs.model.ckpt_path = str(ckpt_path)
-    configs.diffusion.params.steps = 15
-    configs.diffusion.params.sf = 4
+    configs.diffusion.params.sf = scale
     configs.autoencoder.ckpt_path = str(vqgan_path)
 
     return configs
 
-def predict(in_path, task='realsrx4', seed=12345):
-    configs = get_configs(task)
+def predict(in_path, task='realsrx4', seed=12345, scale=4, version='v3'):
+    configs = get_configs(task, version, scale)
     resshift_sampler = ResShiftSampler(
             configs,
+            sf=scale,
             chop_size=256,
             chop_stride=224,
             chop_bs=1,
-            use_fp16=True,
+            use_amp=True,
             seed=seed,
+            padding_offset=configs.model.params.get('lq_size', 64),
             )
 
     out_dir = Path('restored_output')
     if not out_dir.exists():
         out_dir.mkdir()
 
-    resshift_sampler.inference(in_path, out_dir, bs=1, noise_repeat=False)
+    resshift_sampler.inference(
+            in_path,
+            out_dir,
+            mask_path=None,
+            bs=1,
+            noise_repeat=False
+            )
 
     out_path = out_dir / f"{Path(in_path).stem}.png"
     assert out_path.exists(), 'Super-resolution failed!'
@@ -84,11 +147,13 @@ If ResShift is helpful for your work, please help to ⭐ the <a href='https://gi
 ---
 If our work is useful for your research, please consider citing:
 ```bibtex
-@article{yue2023resshift,
+@inproceedings{yue2023resshift,
   title={ResShift: Efficient Diffusion Model for Image Super-resolution by Residual Shifting},
   author={Yue, Zongsheng and Wang, Jianyi and Loy, Chen Change},
-  journal={arXiv preprint arXiv:2307.12348},
-  year={2023}
+  booktitle = {Advances in Neural Information Processing Systems (NeurIPS)},
+  year={2023},
+  volume = {36},
+  pages = {13294--13307},
 }
 ```
 
@@ -107,8 +172,8 @@ demo = gr.Interface(
     inputs=[
         gr.Image(type="filepath", label="Input: Low Quality Image"),
         gr.Dropdown(
-            choices=["realsrx4", "bicsrx4_opencv", "bicsrx4_matlab"],
-            value="realsrx4",
+            choices=["realsr", "bicsr"],
+            value="realsr",
             label="Task",
             ),
         gr.Number(value=12345, precision=0, label="Ranom seed")
@@ -121,15 +186,15 @@ demo = gr.Interface(
     description=description,
     article=article,
     examples=[
-        ['./testdata/RealSet65/0030.jpg',  "realsrx4", 12345],
-        ['./testdata/RealSet65/dog2.png',  "realsrx4", 12345],
-        ['./testdata/RealSet65/bears.jpg', "realsrx4", 12345],
-        ['./testdata/RealSet65/oldphoto6.png', "realsrx4", 12345],
-        ['./testdata/Bicubicx4/lq_matlab/ILSVRC2012_val_00000067.png', "bicsrx4_matlab", 12345],
-        ['./testdata/Bicubicx4/lq_opencv/ILSVRC2012_val_00016898.png', "bicsrx4_opencv", 12345],
+        ['./testdata/RealSet65/0030.jpg',  "realsr", 12345],
+        ['./testdata/RealSet65/dog2.png',  "realsr", 12345],
+        ['./testdata/RealSet65/bears.jpg', "realsr", 12345],
+        ['./testdata/RealSet65/oldphoto6.png', "realsr", 12345],
+        ['./testdata/Bicubicx4/lq_matlab/ILSVRC2012_val_00000067.png', "bicsr", 12345],
+        ['./testdata/Bicubicx4/lq_matlab/ILSVRC2012_val_00016898.png', "bicsr", 12345],
       ]
     )
 
 demo.queue(concurrency_count=4)
-demo.launch(share=False)
+demo.launch(share=True)
 
--- predict.py
+++ predict.py
@@ -8,14 +8,12 @@ from cog import BasePredictor, Input, Path
 
 from sampler import ResShiftSampler
 
-
 class Predictor(BasePredictor):
     def setup(self) -> None:
         """Load the model into memory to make running multiple predictions efficient"""
         self.configs = {
-            "realsrx4": OmegaConf.load("./configs/realsr_swinunet_realesrgan256.yaml"),
-            "bicsrx4_opencv": OmegaConf.load("./configs/bicubic_swinunet_bicubic256.yaml"),
-            "bicsrx4_matlab": OmegaConf.load("./configs/bicubic_swinunet_bicubic256.yaml"),
+            "realsr": OmegaConf.load('./configs/realsr_swinunet_realesrgan256_journal.yaml'),
+            "bicsr": configs = OmegaConf.load('./configs/bicx4_swinunet_lpips.yaml'),
         }
 
     def predict(
@@ -26,12 +24,12 @@ class Predictor(BasePredictor):
             choices=[512, 256], description="Chopping forward.", default=512
         ),
         task: str = Input(
-            choices=["realsrx4", "bicsrx4_opencv", "bicsrx4_matlab"],
+            choices=["realsr", "bicsr"],
             description="Choose a task",
-            default="realsrx4",
+            default="realsr",
         ),
         seed: int = Input(
-            description="Random seed. Leave blank to randomize the seed.", default=None
+            description="Random seed. Leave blank to randomize the seed.", default=12345
         ),
     ) -> Path:
         """Run a single prediction on the model"""
@@ -41,28 +39,39 @@ class Predictor(BasePredictor):
 
         configs = self.configs[task]
 
-        configs.model.ckpt_path = f"weights/resshift_{task}_s15.pth"
-        configs.diffusion.params.steps = 15
+        if task == 'realsr':
+            ckpt_path = f"weights/resshift_realsrx4_s4_v3.pth"
+            configs.model.ckpt_path = ckpt_path
+        else:
+            ckpt_path = f"weights/resshift_bicsrx4_s4.pth"
+            configs.model.ckpt_path = ckpt_path
+        configs.diffusion.params.steps = 4
         configs.diffusion.params.sf = scale
         configs.autoencoder.ckpt_path = f"weights/autoencoder_vq_f4.pth"
-        if task == "bicsrx4_matlab":
-            configs.diffusion.params.kappa = 2.0
 
         chop_stride = 448 if chop_size == 512 else 224
 
         resshift_sampler = ResShiftSampler(
-            configs,
-            chop_size=chop_size,
-            chop_stride=chop_stride,
-            chop_bs=1,
-            use_fp16=True,
-            seed=seed,
-        )
+                configs,
+                sf=scale,
+                chop_size=chop_size,
+                chop_stride=chop_stride,
+                chop_bs=1,
+                use_amp=True,
+                seed=seed,
+                padding_offset=configs.model.params.get('lq_size', 64),
+                )
 
         out_path = "out_dir"
         if os.path.exists(out_path):
             shutil.rmtree(out_path)
-        resshift_sampler.inference(str(image), out_path, bs=1, noise_repeat=False)
+        resshift_sampler.inference(
+                str(image),
+                out_path,
+                mask_path=None,
+                bs=1,
+                noise_repeat=False
+                )
 
         out = "/tmp/out.png"
         shutil.copy(os.path.join(out_path, os.listdir(out_path)[0]), out)
